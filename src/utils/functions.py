5e76c66a5d76891f3ce19615c24af2f05f366a19
Tanzhor_RIK <tanzhor4@gmail.com>
2024-03-31 02:10:44 +0700

    fixed functions.py

--- src/utils/functions.py
+++ src/utils/functions.py
@@ -1,8 +1,10 @@
-def write_data(a, b, file="data/file_data.txt"):
+def write_data(a: int, b: int, file="data/file_data.txt") -> None:
     with open(file, "w") as out_f:
         print(f"starts: {a}\nkilled tanks: {b}", file=out_f)
 
-def get_data(file="data/file_data.txt"):
+
+def get_data(file="data/file_data.txt") -> list:
     with open(file, "r") as in_f:
-        data = [int(x.split(": ")[1]) for x in in_f.readlines() if len(x.strip()) != 0]
+        data = [int(x.split(": ")[1]) for x in in_f.readlines() if
+                len(x.strip()) != 0]
     return data
